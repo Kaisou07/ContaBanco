4a98b7e9f14c1837305632f61186f40c15c21a14
Kaique Ramos <kaisousa13@gmail.com>
2024-02-05 18:45:26 -0300

    Projeto Criado para Aula de Java - trilha

---
+++ src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+ 
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Por favor, digite seu Nome!");
+            String nomeCliente = scanner.next();
+            
+            System.out.println("Por favor, digite a Agência!");
+            String agencia = scanner.next();
+
+            System.out.println("Por favor, digite o número da Conta!");
+            int numero = scanner.nextInt();
+            
+            System.out.println("Por favor, digite o saldo da Conta!");
+            double saldo = scanner.nextDouble(); 
+
+            
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        }
+    }
+    
+}
+
